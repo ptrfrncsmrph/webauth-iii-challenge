d802b893b374a080d3101b72bba7a3eb69447193
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-04 18:18:45 -0400

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext } from "react"
+import { Route, NavLink } from "react-router-dom"
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Users from "./components/Users"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const { Provider: TokenProvider } = createContext(null)
+
+const App = () => {
+  const [token, setToken] = useState(null)
+  const logOut = () => {
+    console.log("Logout clicked")
   }
+
+  return (
+    <TokenProvider value={{ token, setToken }}>
+      <main>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          {" | "}
+          {token ? (
+            <NavLink to="/" onClick={logOut}>
+              Log Out
+            </NavLink>
+          ) : (
+            <NavLink to="/login">Log In</NavLink>
+          )}
+          {" | "}
+          <NavLink to="/signup">Sign Up</NavLink>
+          {" | "}
+          <NavLink to="/users">Users</NavLink>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/users" component={Users} />
+      </main>
+    </TokenProvider>
+  )
 }
 
-export default App;
+export default App
